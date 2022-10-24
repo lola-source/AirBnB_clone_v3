020dd5d8d78f26f4cc9869143c1a85505f03223a
lola-source <99507617+lola-source@users.noreply.github.com>
2022-10-24 22:38:22 +0300

    Update db_storage.py

---
+++ models/engine/db_storage.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+'''
+    Define class DatabaseStorage
+'''
+from os import getenv
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, scoped_session
+import models
+from models.state import State
+from models.city import City
+from models.base_model import Base
+
+
+class DBStorage:
+    '''
+        Create SQLalchemy database
+    '''
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        '''
+            Create engine and link to MySQL databse (hbnb_dev, hbnb_dev_db)
+        '''
+        user = getenv("HBNB_MYSQL_USER")
+        pwd = getenv("HBNB_MYSQL_PWD")
+        host = getenv("HBNB_MYSQL_HOST")
+        db = getenv("HBNB_MYSQL_DB")
+        envv = getenv("HBNB_ENV", "none")
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            user, pwd, host, db), pool_pre_ping=True)
+        if envv == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        '''
+            Query current database session
+        '''
+        db_dict = {}
+
+        if cls is not None and cls is not "":
+            objs = self.__session.query(models.classes[cls]).all()
+            for obj in objs:
+                key = "{}.{}".format(obj.__class__.__name__, obj.id)
+                db_dict[key] = obj
+            return db_dict
+        else:
+            for k, v in models.classes.items():
+                if k != "BaseModel":
+                    objs = self.__session.query(v).all()
+                    if len(objs) > 0:
+                        for obj in objs:
+                            key = "{}.{}".format(obj.__class__.__name__,
+                                                 obj.id)
+                            db_dict[key] = obj
+            return db_dict
+
+    def new(self, obj):
+        '''
+            Add object to current database session
+        '''
+        self.__session.add(obj)
+
+    def save(self):
+        '''
+            Commit all changes of current database session
+        '''
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        '''
+            Delete from current database session
+        '''
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        '''
+            Commit all changes of current database session
+        '''
+        self.__session = Base.metadata.create_all(self.__engine)
+        factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(factory)
+        self.__session = Session()
+
+    def get(self, cls, id):
+        """" A method to retrieve one object
+        grab_obj = self.all(cls).values()
+        for obj in grab_obj:
+            if obj.id == str(id):
+                return obj
+        return None
+        """
+        grab_obj = models.storage.all(cls)
+        for k, v in grab_obj.items():
+            getstr = cls + '.' + id
+            if k == getstr:
+                return (v)
+        return (None)
+
+    def count(self, cls=None):
+        """ A method to count the number of objects in storage """
+        num_obj = models.storage.all(cls)
+        return len(num_obj)
+
+    def close(self):
+        '''
+            Remove private session attribute
+        '''
+        self.__session.close()
